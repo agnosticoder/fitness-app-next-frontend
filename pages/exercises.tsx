e73f88124d97f8e9a3d530cb6b34eb5661cec581
Satinder Singh <agnosticoder@gmail.com>
2022-07-13 23:59:29 -0400

    change exercises page style

--- pages/exercises.tsx
+++ pages/exercises.tsx
@@ -11,9 +11,9 @@ const Exercises = () => {
             <div className='fixed top-14 standalone:top-[88px] left-0 right-0 p-4 bg-zinc-800'>
                 <input className="w-full p-2 rounded bg-zinc-700" placeholder="Search Exercise" value={value} onChange={(e) => setValue(e.currentTarget.value)} />
             </div>
-            <div className="divide-y divide-zinc-600/50 overflow-auto mt-20 h-[calc(100vh-176px)] standalone:h-[calc(100vh-264px)]">
+            <div className="bg-zinc-700/50 p-4 rounded-md divide-y divide-zinc-600/50 overflow-auto mt-16 h-[calc(100vh-250px)] standalone:h-[calc(100vh-264px)] border-[1px] border-zinc-600 text-zinc-300/80">
                 {exercises.map((exercise) => (
-                    <div key={exercise.name} className="h-20">
+                    <div key={exercise.name} className="h-16">
                         <h3 className="text-center">{exercise.name}</h3>
                     </div>
                 ))}
