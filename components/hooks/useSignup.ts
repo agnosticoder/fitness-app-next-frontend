3b2178bfd4cde2540ede4caed4d7eca17b3b52cc
Satinder Singh <agnosticoder@gmail.com>
2022-08-09 07:27:02 -0400

    add login page

---
+++ components/hooks/useSignup.ts
@@ -0,0 +1,36 @@
+import { useMutation } from 'react-query';
+import { config } from '../../config/config';
+import { customFetch } from './useFetch';
+import type { SignupPayload } from '../../../back_end/src/controllers/user';
+import { useRouter } from 'next/router';
+import { useErrorHandler } from 'react-error-boundary';
+
+const useSignup = () => {
+    const handleError = useErrorHandler();
+    const router = useRouter();
+
+    return useMutation(async (payload: SignupPayload) => {
+        const {data, error} = await customFetch(`${config.apiUrl}/user/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        });
+
+        return {data, error};
+    },
+        {
+            onError: (err) => {
+                //* send error to error boundary
+                handleError(err);
+                console.log('useSaveTemplate', err);
+            },
+            onSuccess: () => {
+                // router.push('/');
+            }
+        }
+    );
+};
+
+export default useSignup;
