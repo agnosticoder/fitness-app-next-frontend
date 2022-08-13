b3a6089df161affb126950288448f702a29dc2e5
Satinder Singh <agnosticoder@gmail.com>
2022-08-13 13:59:56 -0400

    fix config

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,6 @@
 //* I am proxying the api routes because of the CORS issue, cors won't let me set the headers in the request if I don't proxy it
-const NEXT_PUBLIC_API_URL = 'http://satinder.local:3000/api';
 const NODE_ENV = process.env.NODE_ENV || 'development';
+const NEXT_PUBLIC_API_URL = NODE_ENV === 'production' ?  'https://fitness-app-next-frontend.vercel.app/api' : 'http://satinder.local:3000/api';
 
 export const IN_PROD = NODE_ENV === 'production';
 
