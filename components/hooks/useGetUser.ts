37e8d95d40af45e828728dc33ec6c3f2619be42a
Satinder Singh <agnosticoder@gmail.com>
2022-08-14 06:36:27 -0400

    style profile page

--- components/hooks/useGetUser.ts
+++ components/hooks/useGetUser.ts
@@ -2,9 +2,14 @@ import { useQuery } from 'react-query';
 import { config } from '../../config/config';
 import { customFetch } from './useFetch';
 
+type Data = {
+    name: string;
+    email: string;
+}
+
 const useGetUser = () => {
     return useQuery(['getUser'], async () => {
-        const {data, error} = await customFetch(`${config.apiUrl}/user/get`);
+        const {data, error} = await customFetch<Data>(`${config.apiUrl}/user/get`);
         if(error) {
             console.log('useGetUser error', error);
             return;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,23 +1,49 @@
 import { GetServerSideProps } from 'next';
+import { CgProfile } from 'react-icons/cg';
 import { customFetch } from '../components/hooks/useFetch';
+import useGetUser from '../components/hooks/useGetUser';
 import useLogout from '../components/hooks/useLogout';
 import { config } from '../config/config';
 
 const Profile = () => {
-    const {mutate} = useLogout();
-
+    const { data: user } = useGetUser();
+    const { mutate } = useLogout();
     const onLogout = async () => {
         mutate();
-    }
+    };
 
     return (
         <div>
-            <button className='inline-block bg-rose-200/70 text-rose-600 py-1 px-2 text-sm font-bold rounded-md border-[1px] border-rose-300/70' onClick={onLogout}>Log out</button>
+            <div className='bg-zinc-700 p-4 rounded-3xl mt-4'>
+                <div className="mt-6">
+                    <span>
+                        <CgProfile className="mx-auto" size={100} />
+                    </span>
+                </div>
+                <div className="text-center mt-6">
+                    <span className='text-xl text-rose-400'>{user?.name}</span>
+                </div>
+                <div className="text-center mt-6">
+                    <span className='text-sm text-rose-400'>{user?.email}</span>
+                </div>
+            </div>
+            <div className="mt-28 text-center">
+                <button
+                    className="inline-block bg-rose-200/70 text-rose-600 py-1 px-2 text-sm font-bold rounded-md border-[1px] border-rose-300/70"
+                    onClick={onLogout}
+                >
+                    Log out
+                </button>
+            </div>
         </div>
     );
-}
+};
 
 export default Profile;
+
+
+
+
 
 export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
     const {data:user, error} = await customFetch(`${config.apiUrl}/user/get`, {
